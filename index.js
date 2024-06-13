4df8a39b258ee28f2b571f81ae660379c354ae42
Heyner Leiva <heynerleiva96@gmail.com>
2024-06-13 11:37:16 -0600

    Agregado del path para abrir la app.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const { patch } = require('./routes/auth');
 require('dotenv').config();
 
 // * Crear el servidor de express
@@ -24,6 +26,11 @@ app.use(express.json());
 // * Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 
 // * Escuchar peticiones
 app.listen(process.env.PORT, () => {
